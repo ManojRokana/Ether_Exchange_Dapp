2c9c3b27122f5cb81670504951fcb52364f12c5e
Manoj Kumar <manojrokana2002@gmail.com>
2022-11-12 13:12:14 +0530

    Add all my files

---
+++ src/utils/constants.js
@@ -0,0 +1,4 @@
+import abi from "./Transactions.json";
+
+export const contractAddress = "0x81019Fc7844f191DCbE5Ae89404fEE11ddB358e0";
+export const contractABI = abi.abi;
---
+++ src/utils/dummyData.js
@@ -0,0 +1,39 @@
+
+export default [
+  {
+    id: 1,
+    url: "https://metro.co.uk/wp-content/uploads/2015/05/pokemon_crying.gif?quality=90&strip=all&zoom=1&resize=500%2C284",
+    message: "hi take your money back",
+    timestamp: "12/21/2021, 1:35:21 PM",
+    addressFrom: "0x81019Fc7844f191DCbE5Ae89404fEE11ddB358e0",
+    amount: "0.01",
+    addressTo: "0x586657A6040dcB8Eda6E29245928A5622Deb6f16",
+  },
+  {
+    id: 2,
+    url: "https://media4.popsugar-assets.com/files/2013/11/07/832/n/1922398/eb7a69a76543358d_28.gif",
+    message: "get your money",
+    timestamp: "12/21/2021, 4:33:21 PM",
+    addressFrom: "00x81019Fc7844f191DCbE5Ae89404fEE11ddB358e0",
+    amount: "0.01",
+    addressTo: "0x586657A6040dcB8Eda6E29245928A5622Deb6f16",
+  },
+  {
+    id: 3,
+    url: "https://acegif.com/wp-content/uploads/gif-shaking-head-38.gif",
+    message: "i hope you are doing good",
+    timestamp: "12/21/2021, 4:33:21 PM",
+    addressFrom: "0x7b8B07517DC4d968Cd31008178fE5318625ca423",
+    amount: "0.01",
+    addressTo: "0x3928b6f5DefBc7e13A51925A8edaF819c73E953e",
+  },
+
+];
+// {
+//   id: 1,
+//   url: "https://metro.co.uk/wp-content/uploads/2015/05/pokemon_crying.gif?quality=90&strip=all&zoom=1&resize=500%2C284",
+//   message: "hi take your money back",
+//   timestamp: "12/21/2021, 4:33:21 PM",
+//   addressFrom: "0xCF8e569A97C423952DdFf902375C7C76549A6A90",
+//   amount: "0.01",
+//   addressTo: "0x8aa395Ab97837576aF9cd6946C79024ef1acfdbE",
